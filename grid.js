4f40b2f094aa235f26f1ade89359b1a511d36875
Heby T Paul <hebytpaul1111@gmail.com>
2022-11-14 21:48:00 +0530

    fixed error

--- grid.js
+++ grid.js
@@ -1,6 +1,6 @@
 const GRID_SIZE = 21
 
-export function randomFoodPosition(){
+export function randomGridPosition(){
     return {
         x: Math.floor(Math.random() *GRID_SIZE) +1,
         y: Math.floor(Math.random() *GRID_SIZE) +1
