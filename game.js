ef78294f86f15dfae52484ca8b67d202e5a33bbb
Heby T Paul <hebytpaul1111@gmail.com>
2022-11-14 15:35:46 +0530

    added draw function

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 import {update as updateSnake, draw as drawSnake , SNAKE_SPEED} from './snake.js'
 
 let lastRenderTime = 0;
-
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime){
     window.requestAnimationFrame(main)
@@ -24,5 +24,6 @@ function update(){
 }
 
 function draw(){
-    drawSnake()
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
 }
--- snake.js
+++ snake.js
@@ -1,9 +1,25 @@
 export const SNAKE_SPEED = 2;
-
+const snakeBody = [
+    {x:10 , y:11},
+    {x:11 , y:11},
+    {x:12 , y:11},
+    {x:13 , y:11},
+    {x:15 , y:11}
+]
 export function update(){
-    console.log('update snake')
+    for(let i =snakeBody.length - 2 ; i >= 0 ; i--){
+        snakeBody[i + 1] = {...snakeBody[i]}
+    }
+    snakeBody[0].x +=1
+    snakeBody[0].y +=0
 }
 
-export function draw(){
-    console.log('draw snake')
+export function draw(gameBoard){
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.y
+        snakeElement.style.gridColumnStart =segment.x
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
+    })
 }
