2cfb0da5a89905d95e0e5123f86084636c543a49
Heby T Paul <hebytpaul1111@gmail.com>
2022-11-14 14:17:18 +0530

    Added Game Loop

---
+++ game.js
@@ -0,0 +1,26 @@
+let lastRenderTime = 0;
+const SNAKE_SPEED = 2;
+
+function main(currentTime){
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime -lastRenderTime )/1000
+    
+    if (secondsSinceLastRender < 1 /SNAKE_SPEED ) return
+
+    console.log(render)
+        
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update(){
+
+}
+
+function draw(){
+    
+}
